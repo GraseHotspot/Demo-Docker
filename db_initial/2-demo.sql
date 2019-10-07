a7b1b20302563db618a4fac886ee1cad98b8d14b
Tim White <tim@whiteitsolutions.com.au>
2019-10-08 06:51:31 +1000

    Do some cleaning of the database we load

--- db_initial/2-demo.sql
+++ db_initial/2-demo.sql
@@ -1,2 +1,3 @@
+TRUNCATE `radmin`.`adminlog`;
 INSERT INTO `radmin`.`auth` VALUES ('guest','8859fcba18be290d598679d7fc225a69c314b9e3871f4d10b',1);
 INSERT INTO `radmin`.`settings` VALUES ('demosite','true');
